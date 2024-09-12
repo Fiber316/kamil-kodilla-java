1d24f6386d8b5906c5504211e5ca3559d67d6735
Kamil Fabianski <x.ray1996@gmail.com>
2024-09-12 22:12:31 +0200

    6.2 Created calculator and test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.calculator;
+
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int addTest = calculator.add(2, 3);
+        if (addTest == 5) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int subtractTest = calculator.subtract(5, 3);
+        if (subtractTest == 2) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class calculator {
+
+    public calculator() {
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
