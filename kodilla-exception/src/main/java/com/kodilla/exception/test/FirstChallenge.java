cacb5a3a9a0c6bcb83693908565eac99311fca41
Kamil Fabianski <x.ray1996@gmail.com>
2024-09-28 21:30:12 +0200

    8.2 Task done

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -7,6 +7,25 @@ public class FirstChallenge {
             throw new ArithmeticException();
         }
         return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            double result = firstChallenge.divide(3, 0);
+        System.out.println(result);
+    } catch (ArithmeticException e) {
+        System.out.println(e);
+        } finally {
+            System.out.println("End of program");
+        }
+    }
+}
+
 //        try (Stream<String> fileLines = Files.lines(Paths.get("tego-pliku-nie-ma.txt"))) {
 //            fileLines.forEach(System.out::println);
 //        } catch (IOException e) {
@@ -14,19 +33,3 @@ public class FirstChallenge {
 //        } finally {
 //            System.out.println("I am gonna be here... always!");
 //        }
-//    }
-//
-    }
-}
-//
-//    /**
-//     * This main can throw an ArithmeticException!!!
-//     * @param args
-//     */
-//    public static void main(String[] args) {
-//        FirstChallenge firstChallenge = new FirstChallenge();
-//        double result = firstChallenge.divide(3, 0);
-//
-//        System.out.println(result);
-//    }
-//}
