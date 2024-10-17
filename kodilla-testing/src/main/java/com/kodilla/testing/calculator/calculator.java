26fcc73bab2a9ad5956f09d7517e89b3bd1f7865
Kamil F <x.ray1996@gmail.com>
2024-10-17 23:09:51 +0200

    test

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/calculator.java
@@ -11,4 +11,6 @@ public class calculator {
     public static int subtract(int a, int b) {
         return a - b;
     }
+    public static int multiply(int a, int b) { return a * b; }
+    public static int divide(int a, int b) { return a / b; }
 }
