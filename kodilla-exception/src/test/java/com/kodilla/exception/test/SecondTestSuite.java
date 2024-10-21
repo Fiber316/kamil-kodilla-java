d29af38c2da3256ef296de5aedfa9be09ac902a9
Kamil F <x.ray1996@gmail.com>
2024-10-21 16:19:48 +0200

    8.3 fix

--- kodilla-exception/src/test/java/com/kodilla/exception/test/SecondTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/SecondTestSuite.java
@@ -23,7 +23,7 @@ class SecondTestSuite {
 
         // When & Then
         assertThrows(Exception.class, () -> {
-            secondChallenge.probablyIWillThrowException(2.0, 1.0);
+            secondChallenge.probablyIWillThrowException(2.0000001, 1.0);
         });
     }
 
@@ -34,7 +34,7 @@ class SecondTestSuite {
 
         // When & Then
         assertThrows(Exception.class, () -> {
-            secondChallenge.probablyIWillThrowException(0.9, 1.0);
+            secondChallenge.probablyIWillThrowException(0.999999999, 1.0);
         });
     }
 
